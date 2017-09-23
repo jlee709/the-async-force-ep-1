ae2901df5fbbbc175144b650d41b9f55673b7ccb
Justin Lee <leejwg.3@gmail.com>
2017-09-22 16:19:14 -1000

    vader planet working

--- js/app.js
+++ js/app.js
@@ -2,27 +2,34 @@
 // var myHeader = XMLHttpRequest.getResponseHeader(name);
 // console.log(myHeader);
 (function(){
-console.log('iffe fired')
+  console.log('iffe fired')
 
   let oReq = new XMLHttpRequest(); // vanilla
   oReq.addEventListener('load', function(){
     let personObj = JSON.parse(this.responseText);
     document.getElementById('person4Name').innerHTML = personObj.name;
+    console.log(personObj.name)
+    
+    // do another request 
+    let homeworldReq = new XMLHttpRequest();
+    homeworldReq.addEventListener('load',function(){
+      //happens after 2nd request
+      let homeworldObj = JSON.parse(this.responseText);
+      document.getElementById('person4HomeWorld').innerHTML = homeworldObj.name;
+      console.log(homeworldObj)
+    });
+    homeworldReq.open('GET', personObj.homeworld);
+    homeworldReq.send();
+
   });
+
+
+
+
+// first request 
   oReq.open('GET','https://swapi.co/api/people/4');
   oReq.send();
-
-
-   let homeWorldReq = new XMLHttpRequest(); // vanilla
-  homeWorldReq.addEventListener('load', function(){
-    let personObj = JSON.parse(this.responseText);
-    document.getElementById('person4HomeWorld').innerHTML = homeWorldObj.name;
-    console.log(homeWorldObj);
-  });
-  homeWorldReq.open('GET', personObj.homeWorld);
-  homeWorldReq.send();
-
-})();
+}());
 
 
 
